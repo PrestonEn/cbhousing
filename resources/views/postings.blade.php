30fcd6c2b7d151d2432f37ed500369d9b6226339
James Earle <j_earle@hotmail.com>
2015-06-26 16:37:24 -0400

    postings change

--- resources/views/postings.blade.php
+++ resources/views/postings.blade.php
@@ -10,43 +10,43 @@
     <h1>Take a look at these!</h1>
 </section>
 
-	<section id="portfolio">
-	    <div class="container">
-            <div class="row">
-<?php $flag = true; ?>
-@foreach($data['pages'] as $property)
-@if($property->rooms>0)
-<?php if($flag){
-    $class = "even";
-}else{
-    $class = "odd";
-} ?>
-                <div class="col-md-4 col-sm-6 property-item">
-                                        <h5 class="pricecell">{{$data[$property->id]}} rooms</h5>
-                    @if($flag)
-                        <div class="inner bluebox">
-                    @else
-                        <div class="inner orangebox">
-                    @endif
-                    <a href="#portfolioModal6" class="portfolio-link" data-toggle="modal">
-                        <img src="/{{$property->thumbnail}}" class="img-responsive port-img" alt="">
-                    </a>
-
-
-                    <div class="portfolio-caption">
-                        <a href="{{url('properties/'.$property->id)}}">
-                           <h4 class='title'>{{$property->title}}</h4>
-                           <h4>{{$property->address}}</h4>
+<section id="portfolio">
+    <div class="container">
+        <div class="row">
+        
+        <?php $flag = true; ?>
+        @foreach($data['pages'] as $property)
+            @if($property->rooms>0)
+                <?php if($flag){
+                    $class = "even";
+                }else{
+                    $class = "odd";
+                } ?>
+                    <div class="col-md-4 col-sm-6 property-item">
+                        <h5 class="pricecell">{{$data[$property->id]}} room(s)</h5>
+                        
+                        @if($flag)
+                            <div class="inner bluebox">
+                        @else
+                            <div class="inner orangebox">
+                        @endif
+                        <a href="#portfolioModal6" class="portfolio-link" data-toggle="modal">
+                            <img src="/{{$property->thumbnail}}" class="img-responsive port-img" alt="">
                         </a>
+
+                        <div class="portfolio-caption">
+                            <a href="{{url('properties/'.$property->id)}}">
+                               <h4 class='title'>{{$property->title}}</h4>
+                               <h4>{{$property->address}}</h4>
+                            </a>
+                        </div>
                     </div>
                 </div>
-                </div>
-<?php $flag = !$flag ?>
-@endif
-@endforeach
-            </div>
-        </div>
-    </section>
+                <?php $flag = !$flag ?>
+            @endif
+        @endforeach
+    </div>
+</section>
 
 <div class="row paginator-links">
     <div class="paginator-links">
